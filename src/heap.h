6884dff4505cfd9666e4e5e60944575fd415e024
mateuszkowalke <mateusz.kowalke@gmail.com>
2024-01-24 09:30:21 +0100

    heap

--- src/heap.h
+++ src/heap.h
@@ -2,81 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include "dyn_arr.h"
 
-#define decl_dyn_arr_type(T) \
-    typedef struct { \
-        T *arr; \
-        size_t len; \
-        size_t cap; \
-    } dyn_##T##_arr_t; \
-    dyn_##T##_arr_t new_dyn_##T##_arr(size_t size) { \
-        T *arr = malloc(sizeof(T) * size); \
-        if (arr == NULL) { \
-            perror("Error reallocating dynamic array(new)"); \
-            exit(1); \
-        } \
-        return (dyn_##T##_arr_t){.arr = arr, .len = 0, .cap = size}; \
-    } \
-    void push_##T(dyn_##T##_arr_t *dyn_arr, T el) { \
-        dyn_arr->arr[dyn_arr->len++] = el; \
-        if (dyn_arr->len == dyn_arr->cap) { \
-            dyn_arr->cap *= 2; \
-            dyn_arr->arr = realloc(dyn_arr->arr, sizeof(T) * dyn_arr->cap); \
-            if (dyn_arr->arr == NULL) { \
-                perror("Error reallocating dynamic array(push)"); \
-                exit(1); \
-            } \
-        } \
-    } \
-    T pop_##T(dyn_##T##_arr_t *dyn_arr) { \
-        if (dyn_arr->len == 0) { \
-            fprintf(stderr, "Trying to pop from empty dynamic array\n"); \
-            exit(1); \
-        } \
-        T val = dyn_arr->arr[--dyn_arr->len]; \
-        if (dyn_arr->len < dyn_arr->cap / 2) { \
-            dyn_arr->cap /= 2; \
-            dyn_arr->arr = realloc(dyn_arr->arr, sizeof(T) * dyn_arr->cap); \
-            if (dyn_arr->arr == NULL) { \
-                perror("Error reallocating dynamic array(pop)"); \
-                exit(1); \
-            } \
-        } \
-        return val; \
-    } \
-    T insert_##T##_at(dyn_##T##_arr_t dyn_arr, size_t i, T el) { \
-        if (i >= dyn_arr.len || i < 0) { \
-            fprintf(stderr, "Dynamic array index out of bounds(insert_at): dyn_arr.len = %ld; i = %ld\n", dyn_arr.len, i); \
-            exit(1); \
-        } \
-        return dyn_arr.arr[i] = el; \
-    } \
-    T T##_at(dyn_##T##_arr_t dyn_arr, size_t i) { \
-        if (i >= dyn_arr.len || i < 0) { \
-            fprintf(stderr, "Dynamic array index out of bounds(at): dyn_arr.len = %ld; i = %ld\n", dyn_arr.len, i); \
-            exit(1); \
-        } \
-        return dyn_arr.arr[i]; \
-    } \
-    void swap_##T(dyn_##T##_arr_t dyn_arr, size_t i, size_t j) { \
-        if (i >= dyn_arr.len || i < 0) { \
-            fprintf(stderr, "Dynamic array index out of bounds(swap): dyn_arr.len = %ld; i = %ld\n", dyn_arr.len, i); \
-            exit(1); \
-        } \
-        if (j >= dyn_arr.len || j < 0) { \
-            fprintf(stderr, "Dynamic array index out of bounds(swap): dyn_arr.len = %ld; j = %ld\n", dyn_arr.len, j); \
-            exit(1); \
-        } \
-        T tmp = dyn_arr.arr[i]; \
-        dyn_arr.arr[i] = dyn_arr.arr[j]; \
-        dyn_arr.arr[j] = tmp; \
-    } \
+#ifndef HEAP
+#define HEAP
 
 #define decl_min_heap_type(T) \
     decl_dyn_arr_type(T); \
     typedef dyn_##T##_arr_t T##_min_heap_t; \
     T##_min_heap_t new_##T##_min_heap() { \
         return (T##_min_heap_t)new_dyn_##T##_arr(64); \
+    } \
+    int get_left_child_##T##_min_heap(T##_min_heap_t h, size_t parent_i, T *el) { \
+        if (parent_i * 2 + 1 >= h.len) { \
+            return -1; \
+        } \
+        *el = T##_at(h, parent_i * 2 + 1); \
+        return 0; \
+    } \
+    int get_right_child_##T##_min_heap(T##_min_heap_t h, size_t parent_i, T *el) { \
+        if (parent_i * 2 + 2 >= h.len) { \
+            return -1; \
+        } \
+        *el = T##_at(h, parent_i * 2 + 2); \
+        return 0; \
     } \
     void push_##T##_min_heap(T##_min_heap_t *h, T el) { \
         size_t i = h->len; \
@@ -88,4 +37,29 @@
             j = i < 2 ? 0 : (i - 1) / 2; \
         } \
     } \
+    T pop_##T##_min_heap(T##_min_heap_t *h) { \
+        if (h->len == 1) { \
+            return pop_##T(h); \
+        } \
+        T res = T##_at(*h, 0); \
+        insert_##T##_at(*h, 0, pop_##T(h)); \
+        size_t parent_i = 0; \
+        T left, right; \
+        while (get_left_child_##T##_min_heap(*h, parent_i, &left) != -1) { \
+            if (get_right_child_##T##_min_heap(*h, parent_i, &right) != -1) { \
+                if (left < right) { \
+                    swap_##T(*h, parent_i, parent_i * 2 + 1); \
+                    parent_i = parent_i * 2 + 1; \
+                } else { \
+                    swap_##T(*h, parent_i, parent_i * 2 + 2); \
+                    parent_i = parent_i * 2 + 2; \
+                }\
+            } else { \
+                swap_##T(*h, parent_i, parent_i * 2 + 1); \
+                parent_i = parent_i * 2 + 1; \
+            } \
+        } \
+        return res; \
+    } \
 
+#endif
--- tests/test_heap.c
+++ tests/test_heap.c
@@ -29,8 +29,54 @@ Test(min_heap, should_properly_push_lots_of_data_on_min_heap) {
   push_int_min_heap(&h, 4);
   push_int_min_heap(&h, 2);
   cr_expect(h.arr[0] == 2);
-  for (int i = 0; i < h.len; i++) {
-      printf("%d, ", h.arr[i]);
-  }
-  printf("\n");
+}
+
+Test(min_heap, should_properly_pop_data_from_min_heap) {
+  int_min_heap_t h = new_int_min_heap();
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 2);
+  cr_expect(pop_int_min_heap(&h) == 2);
+  cr_expect(pop_int_min_heap(&h) == 4);
+}
+
+Test(min_heap, should_properly_pop_all_data_from_min_heap) {
+  int_min_heap_t h = new_int_min_heap();
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 2);
+  cr_expect(pop_int_min_heap(&h) == 2);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(h.len == 0);
+}
+
+Test(min_heap, should_properly_pop_lots_of_data_from_min_heap) {
+  int_min_heap_t h = new_int_min_heap();
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 8);
+  push_int_min_heap(&h, 3);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 5);
+  push_int_min_heap(&h, 9);
+  push_int_min_heap(&h, 7);
+  push_int_min_heap(&h, 6);
+  push_int_min_heap(&h, 6);
+  push_int_min_heap(&h, 4);
+  push_int_min_heap(&h, 2);
+  cr_expect(pop_int_min_heap(&h) == 2);
+  cr_expect(pop_int_min_heap(&h) == 3);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(pop_int_min_heap(&h) == 4);
+  cr_expect(pop_int_min_heap(&h) == 5);
+  cr_expect(pop_int_min_heap(&h) == 6);
+  cr_expect(pop_int_min_heap(&h) == 6);
 }
